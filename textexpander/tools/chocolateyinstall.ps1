12b108d817128df1ff25b4f9ad52c50fbdc84c55
davidfordtn <107569062+davidfordtn@users.noreply.github.com>
2024-10-21 12:47:30 -0700

    Add files via upload

---
+++ textexpander/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+
+  url          = 'https://rest-prod.tenet.textexpander.com/download?platform=windows'
+  checksum     = '8939a0ce66c5919c8e56578c42c658be3bab171bf88cbc42e688a50e671509d6'
+  checksumType = 'sha256'
+
+  silentArgs   = '/s'
+}
+
+Install-ChocolateyPackage @packageArgs
